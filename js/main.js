76b39c2ff74983cd6661bc390e8d48fae3184e9f
frsdjefaflia <djefaflia.fares@gmail.com>
2021-10-26 18:15:41 +0100

    add all

---
+++ js/main.js
@@ -0,0 +1,14 @@
+// let login = document.querySelector(".login input");
+    // let user = document.querySelector(".user input");
+    // let pass = document.querySelector(".pass input");
+
+
+    // login.addEventListener("click", () => {
+    //   if (user.value === "frscodes" && pass.value === "frscodes"){
+    //     window.location.href = "/pull.html";
+    //   } else{
+    //     alert("ERROR IN PASSWORD OR USERNAME, PLEASE TRY: frscodes")
+    //     document.querySelector(".user").style.border = "solid #ff0000 2px";
+    //     document.querySelector(".pass").style.border = "solid #ff0000 2px";
+    //   };
+    // });
