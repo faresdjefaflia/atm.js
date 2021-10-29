3b627f297c8c9585368e7c28b007c277bf27cd7c
frsdjefaflia <djefaflia.fares@gmail.com>
2021-10-29 20:00:22 +0100

    add money design

--- js/main.js
+++ js/main.js
@@ -1,14 +1,63 @@
-// let login = document.querySelector(".login input");
-    // let user = document.querySelector(".user input");
-    // let pass = document.querySelector(".pass input");
+let m1000 = document.createRange().createContextualFragment(`
+  <div class="m-1000">
+  <div>
+    <h1>1000$</h1>
+  </div>
+  <div class="m-bx">
+    <h1>$</h1>
+    <div></div>
+  </div>
+  </div>
+`);
 
+let m500 = document.createRange().createContextualFragment(`
+  <div class="m-500">
+  <div>
+    <h1>500$</h1>
+  </div>
+  <div class="m-bx">
+    <h1>$</h1>
+    <div></div>
+  </div>
+  </div>
+`);
 
-    // login.addEventListener("click", () => {
-    //   if (user.value === "frscodes" && pass.value === "frscodes"){
-    //     window.location.href = "pull";
-    //   } else{
-    //     alert("ERROR IN PASSWORD OR USERNAME, PLEASE TRY: frscodes")
-    //     document.querySelector(".user").style.border = "solid #ff0000 2px";
-    //     document.querySelector(".pass").style.border = "solid #ff0000 2px";
-    //   };
-    // });
+let m100 = document.createRange().createContextualFragment(`
+  <div class="m-100">
+  <div>
+    <h1>100$</h1>
+  </div>
+  <div class="m-bx">
+    <h1>$</h1>
+    <div></div>
+  </div>
+  </div>
+`);
+
+let m10 = document.createRange().createContextualFragment(`
+  <div class="m-10">
+  <div>
+    <h1>50$</h1>
+  </div>
+  <div class="m-bx">
+    <h1>$</h1>
+    <div></div>
+  </div>
+  </div>
+`)
+let m1 = document.createRange().createContextualFragment(`
+  <div class="m-1">
+  <div>
+    <h1>1$</h1>
+  </div>
+  <div class="m-bx">
+    <h1>$</h1>
+    <div></div>
+  </div>
+  </div>
+`);
+
+let amount = document.querySelector('.amount input');
+let pull = document.querySelector('.pull input');
+let boxPull = document.querySelector('.box-pull');
+
