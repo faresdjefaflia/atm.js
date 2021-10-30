5b5781da4db412fe7fc1bb6c287df5ff94907f7e
frsdjefaflia <djefaflia.fares@gmail.com>
2021-10-30 20:05:30 +0100

    add framework swal2 for error

--- js/main.js
+++ js/main.js
@@ -1,3 +1,28 @@
+
+//FOR PAGE LOGIN
+
+let login = document.querySelector(".login input");
+let user = document.querySelector(".user input");
+let pass = document.querySelector(".pass input");
+
+login.addEventListener("click", () => {
+  if (user.value === "admin" && pass.value === "admin"){
+    window.location.href = "pull";
+  } else{
+    // SWAL2
+    // THANKS: https://sweetalert2.github.io/
+    Swal.fire({
+      icon: 'error',
+      title: 'Oops...',
+      text: 'ERROR IN PASSWORD OR USERNAME, PLEASE TRY: admin',
+    })
+    document.querySelector(".user").style.border = "solid #ff0000 2px";
+    document.querySelector(".pass").style.border = "solid #ff0000 2px";
+  };
+});
+
+
+
 let m1000 = document.createRange().createContextualFragment(`
   <div class="m-1000">
   <div>
